0842fce14426313e949bf22a7c6282214e9079c5
Morgan Garcia <morgangarcia1977@gmail.com>
2019-03-02 21:14:38 -0500

    2push

---
+++ burger/config/connection.js
@@ -0,0 +1,19 @@
+// Pull in required dependencies
+const mysql = require('mysql');
+
+// Create the MySQL connection object
+const connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	})
+};
